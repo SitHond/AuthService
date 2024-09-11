0776b61f8925e95b170e06c19cd4b4ffe4e7d0fa
SitHond <maksonpavloff2004@yandex.ru>
2024-09-12 02:01:28 +0300

    Подключение к бд и модель

---
+++ dbMal/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using dbMal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuthService.Models
+{
+    [Table(nameof(UserContext.Users))]
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Email { get; set; }
+        [MaxLength(255)]
+        public string Password { get; set; }
+    }
+}
---
+++ dbMal/UsersContext.cs
@@ -0,0 +1,14 @@
+﻿using AuthService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dbMal
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=DBMal;User Id=postgres;Password=qwerty");
+        }
+    }
+}
